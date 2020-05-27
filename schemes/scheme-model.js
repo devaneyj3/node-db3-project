1bd55fb12ecd0dea403305fed1baba645adace92
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-27 00:02:35 -0400

    find by scheme id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,6 +4,11 @@ function find() {
     return db('schemes')
 }
 
+function findById(id) {
+    return db('schemes').where({id}).first()
+}
+
 module.exports = {
-    find
+    find,
+    findById
 }
